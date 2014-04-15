fd84355a16efeabf9ead41cc001e923ede4453e5
Justin Wienckowski <justin@seatme.com>
2014-04-14 18:01:00 -0700

    Make click() more flexible.  Adddismiss_popover() action.

--- pangolin/actions.rb
+++ pangolin/actions.rb
@@ -4,7 +4,13 @@ module Pangolin
     DSL_METHODS = [:click, :single_tap, :within_alert, :fill_in, :dismiss_popover]
 
     def click(*args)
-      find(*args).tap(&:click)
+      opts = args.last if args.last.kind_of?(Hash)
+
+      if args.count.zero?
+        mobile_tap opts[:x], opts[:y]
+      else
+        find(*args).tap(&:click)
+      end
     end
 
     alias_method :single_tap, :click
@@ -17,6 +23,22 @@ module Pangolin
         raise Pangolin::ElementNotFound.new("No alert found")
       end
     end
-  end
 
+    def dismiss_popover
+      mobile_tap(0.99, 0.99)
+
+      wait = Selenium::WebDriver::Wait.new(:timeout => 5) # seconds
+      wait.until { driver.find_elements(:tag_name, 'popover').empty?  }
+    end
+
+    #
+    # Appium Mobile Extensions
+    # 
+
+    def mobile_tap(x, y)
+      window = driver.find_element(:tag_name, 'window')
+      driver.execute_script 'mobile: tap', :x => x, :y => y, :element => window.ref
+    end
+
+  end
 end
